e46ece90b71291a7160f0779bd493c5fcee00436
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-13 12:07:01 -0400

    dashboard view

--- playgroup/views.py
+++ playgroup/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from playgroup.models import PlayGroup
 
 @login_required
 def dashboard(request):
-    pass
+    owned_playgroups = PlayGroup.objects.filter(owner=request.user)
+    joined_playgroups = request.user.playgroups.all()
+    return render(request, 'playgroup/dashboard.html', {
+        'owned_playgroups': owned_playgroups,
+        'joined_playgroups': joined_playgroups,
+    })
 
 @login_required
 def create(request):
