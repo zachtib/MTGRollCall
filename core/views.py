168cadf64de509f9a0beab455eac061643e30a98
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-13 10:24:34 -0400

    core app and autoconfigure

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+
+import os
+
+def autoconfigure(request):
+    users = User.objects.all()
+    if users.count() != 0:
+        raise Http404()
+
+    username = os.environ.get('ADMIN_USERNAME')
+    email = os.environ.get('ADMIN_EMAIL')
+    password = os.environ.get('ADMIN_PASSWORD')
+    if not username or not password:
+        raise Http404()
+
+    admin = User(username=username, email=email)
+    admin.set_password(password)
+    admin.is_superuser = True
+    admin.is_staff = True
+    admin.save()
+
+    return HttpResponse('Admin user created')
--- mtgrollcall/urls.py
+++ mtgrollcall/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import autoconfigure
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('autoconfigure/', autoconfigure),
 ]
