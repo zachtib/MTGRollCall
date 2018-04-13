76a13c8f5732371d3e37f952ec57dfa514e7ae98
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-13 14:17:37 -0400

    events and datepicker

--- mtgrollcall/settings.py
+++ mtgrollcall/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'core',
     'playgroup',
+    'event',
 ]
 
 MIDDLEWARE = [
--- mtgrollcall/urls.py
+++ mtgrollcall/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('playgroup/', include('playgroup.urls')),
+    path('event/', include('event.urls')),
     path('autoconfigure/', core_views.autoconfigure),
     path('', core_views.dashboard),
 ]
