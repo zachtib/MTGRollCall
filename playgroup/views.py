cf2f794bcd5e63346864e6e3ac276acf2888e0a6
Zach Tibbitts <zachtib@gmail.com>
2018-04-22 13:51:32 -0400

    just pep8 things

--- playgroup/views.py
+++ playgroup/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 
 from playgroup.models import PlayGroup
 
+
 @login_required
 def dashboard(request):
     owned_playgroups = PlayGroup.objects.filter(owner=request.user)
@@ -13,13 +14,15 @@ def dashboard(request):
         'joined_playgroups': joined_playgroups,
     })
 
+
 @login_required
 def create(request):
     pass
 
+
 @login_required
 def details(request, id):
-    playgroup: PlayGroup = get_object_or_404(PlayGroup, pk=id)
+    playgroup = get_object_or_404(PlayGroup, pk=id)
     if not playgroup.viewable_by(request.user):
         raise Http404()
     return render(request, 'playgroup/details.html', {
