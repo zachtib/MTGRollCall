ed913e22bf9749b7ae88560313570f98fed25457
Zach Tibbitts <zachtib@gmail.com>
2018-04-22 13:48:04 -0400

    strip down models to work on MVP

--- playgroup/models.py
+++ playgroup/models.py
@@ -5,12 +5,9 @@ from django.db import models
 class PlayGroup(models.Model):
     name = models.CharField(max_length=80)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    members = models.ManyToManyField(User, through='Membership', related_name='playgroups')
 
     def viewable_by(self, user: User):
         if self.owner == user:
-            return True
-        elif self in user.playgroups.all():
             return True
         return False
 
@@ -19,8 +16,9 @@ class PlayGroup(models.Model):
 
 
 class Membership(models.Model):
-    playgroup = models.ForeignKey(PlayGroup, related_name='membership', on_delete=models.CASCADE)
-    member = models.ForeignKey(User, related_name='membership', on_delete=models.CASCADE)
+    playgroup = models.ForeignKey(PlayGroup, related_name='membership',
+                                  on_delete=models.CASCADE)
+    email = models.EmailField()
 
     def __str__(self):
-        return f'{self.member} is in {self.playgroup}'
+        return f'{self.email} is in {self.playgroup}'
