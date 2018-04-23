8e1d692f02440f65dcca90cf26fa6c534eb4063c
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-23 09:33:38 -0400

    url names

--- playgroup/urls.py
+++ playgroup/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from playgroup import views
 
+app_name = 'playgroup'
 urlpatterns = [
-    path('new/', views.create),
-    path('<int:group_id>/', views.details),
-    path('', views.dashboard),
+    path('', views.dashboard, name='home'),
+    path('create/', views.create, name="create"),
+    path('<int:group_id>/', views.details, name="details"),
 ]
