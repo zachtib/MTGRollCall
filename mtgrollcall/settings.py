811dabf2157f6597d2402621a9c16f876026dfea
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-24 12:43:24 -0400

    logging

--- mtgrollcall/settings.py
+++ mtgrollcall/settings.py
@@ -145,3 +145,19 @@ else:
 
 import django_heroku
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
