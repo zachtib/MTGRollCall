6ca8df707d8bbb713a7999b2f35ee24ad1f764db
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-27 11:03:52 -0400

    cleanup

--- core/templatetags/core/tags.py
+++ core/templatetags/core/tags.py
@@ -8,19 +8,17 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def nav_element(context, url, text):
+def nav(context, url, text):
     try:
-        reversed = reverse(url)
-        if reversed == '/':
-            # Special case for landing page
-            pattern = '^%s$' % reversed
-        else:
-            pattern = '^%s' % reversed
+        url = reverse(url)
     except NoReverseMatch:
-        pattern = url
+        pass
+
+    # Special case for landing page
+    pattern = f'^{url}$' if url == '/' else f'^{url}'
 
     path = context['request'].path
-    active = True if re.search(pattern, path) else False
-    return mark_safe(f'''<li class="nav-item{" active" if active else ""}">
-                   <a class="nav-link" href="{reverse(url)}">{text}</a>
-               </li>''')
+    active = re.search(pattern, path)
+    return mark_safe(f'<li class="nav-item{" active" if active else ""}">'
+                     f'<a class="nav-link" href="{url}">{text}</a>'
+                     f'</li>')
