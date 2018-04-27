8056bf856a96c2a7ed314ed62833b32adc9116a2
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-27 15:46:31 -0400

    better admin

--- event/admin.py
+++ event/admin.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 
 from event.models import Event, Invitation
 
+
 class InvitationInline(admin.TabularInline):
     model = Invitation
+
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     inlines = [
         InvitationInline,
     ]
+    list_display = ('name', 'playgroup', 'date')
+    date_hierarchy = 'date'
+
 
 @admin.register(Invitation)
 class InvitationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('event', 'member', 'response', 'sent')
